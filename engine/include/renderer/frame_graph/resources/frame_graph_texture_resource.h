3ad7bbbbae74ea2b7879dfbf45f5793b45895f86
Raj <amritrjakacabu015@gmail.com>
2026-02-23 21:42:35 -0500

    Preparing for creating frame graph resources.

---
+++ engine/include/renderer/frame_graph/resources/frame_graph_texture_resource.h
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace Vulkyrie::Renderer {
+
+    class FrameGraphTextureResource {
+        public:
+            struct Descriptor {};
+
+            void Create(const Descriptor &descriptor, void *context) {
+            }
+
+            void Destroy(const Descriptor &descriptor, void *context) {
+            }
+    };
+
+} // namespace Vulkyrie::Renderer
