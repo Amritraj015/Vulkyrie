52b1b39be6c8216b3a7c336aaa3574a3c9e0d394
Raj <amritrjakacabu015@gmail.com>
2026-03-20 23:14:39 -0400

    Some code cleanup.

--- examples/sandbox/src/sandbox_layer_sphere.h
+++ examples/sandbox/src/sandbox_layer_sphere.h
@@ -12,47 +12,76 @@ namespace Sandbox {
 
     class World final {
         public:
-            World(std::array<glm::vec3, 8> walls, f32 sphereRadius, std::array<glm::vec3, 2> spherePositionAndSpeed)
-                : walls(walls)
-                , sphereRadius(sphereRadius)
-                , spherePositionAndSpeed(spherePositionAndSpeed) {
+            World() = default;
+
+            World(std::array<std::pair<glm::vec3, f32>, 4> planes, std::array<std::tuple<glm::vec3, glm::vec3, f32>, 1> spheres)
+                : spheres(spheres) {
+
+                const auto &[rightWallNormal, rightWallDistance] = planes[0];
+                const glm::vec3 rightWallPoint = glm::vec3(rightWallNormal * rightWallDistance * -1.0f);
+
+                const auto &[leftWallNormal, leftWallDistance] = planes[1];
+                const glm::vec3 leftWallPoint = glm::vec3(leftWallNormal * leftWallDistance * -1.0f);
+
+                const auto &[topWallNormal, topWallDistance] = planes[2];
+                const glm::vec3 topWallPoint = glm::vec3(topWallNormal * topWallDistance * -1.0f);
+
+                const auto &[bottomWallNormal, bottomWallDistance] = planes[3];
+                const glm::vec3 bottomWallPoint = glm::vec3(bottomWallNormal * bottomWallDistance * -1.0f);
+
+                walls = {
+                    std::make_pair(rightWallNormal, rightWallPoint),
+                    std::make_pair(leftWallNormal, leftWallPoint),
+                    std::make_pair(topWallNormal, topWallPoint),
+                    std::make_pair(bottomWallNormal, bottomWallPoint),
+                };
             }
 
             void Update(Timestep dt) {
-                const auto distanceRight = glm::dot((spherePositionAndSpeed[0] - walls[0]), walls[1]);
-                const auto distanceLeft = glm::dot((spherePositionAndSpeed[0] - walls[2]), walls[3]);
-                const auto distanceTop = glm::dot((spherePositionAndSpeed[0] - walls[4]), walls[5]);
-                const auto distanceBottom = glm::dot((spherePositionAndSpeed[0] - walls[6]), walls[7]);
+                for (auto &[spherePosition, sphereSpeed, sphereRadius] : spheres) {
+                    const f32 distanceRight = glm::dot((spherePosition - walls[0].second), walls[0].first);
+                    const f32 distanceLeft = glm::dot((spherePosition - walls[1].second), walls[1].first);
+                    const f32 distanceTop = glm::dot((spherePosition - walls[2].second), walls[2].first);
+                    const f32 distanceBottom = glm::dot((spherePosition - walls[3].second), walls[3].first);
 
-                if (distanceRight <= sphereRadius) {
-                    if (spherePositionAndSpeed[1].x > 0.0f) {
-                        spherePositionAndSpeed[1] *= glm::vec3(-1.0f, 1.0f, 1.0f);
+                    if (distanceRight <= sphereRadius && sphereSpeed.x > 0.0f) {
+                        sphereSpeed *= glm::vec3(-1.0f, 1.0f, 1.0f);
+                    } else if (distanceLeft <= sphereRadius && sphereSpeed.x < 0.0f) {
+                        sphereSpeed *= glm::vec3(-1.0f, 1.0f, 1.0f);
+                    } else if (distanceTop <= sphereRadius && sphereSpeed.y > 0.0f) {
+                        sphereSpeed *= glm::vec3(1.0f, -1.0f, 1.0f);
+                    } else if (distanceBottom <= sphereRadius && sphereSpeed.y < 0.0f) {
+                        sphereSpeed *= glm::vec3(1.0f, -1.0f, 1.0f);
                     }
-                } else if (distanceTop <= sphereRadius) {
-                    if (spherePositionAndSpeed[1].y > 0.0f) {
-                        spherePositionAndSpeed[1] *= glm::vec3(1.0f, -1.0f, 1.0f);
-                    }
-                } else if (distanceLeft <= sphereRadius) {
-                    if (spherePositionAndSpeed[1].x < 0.0f) {
-                        spherePositionAndSpeed[1] *= glm::vec3(-1.0f, 1.0f, 1.0f);
-                    }
-                } else if (distanceBottom <= sphereRadius) {
-                    if (spherePositionAndSpeed[1].y < 0.0f) {
-                        spherePositionAndSpeed[1] *= glm::vec3(1.0f, -1.0f, 1.0f);
-                    }
+
+                    spherePosition += sphereSpeed * dt.GetSeconds();
                 }
-
-                spherePositionAndSpeed[0] += spherePositionAndSpeed[1] * dt.GetSeconds();
             }
 
             const glm::vec3 GetSpherePosition() const {
-                return spherePositionAndSpeed[0];
+                return std::get<0>(spheres[0]);
             }
+            //
+            // enum Wall { Left, Right, Top, Bottom };
+            //
+            // const std::tuple<glm::mat4, glm::vec3, glm::vec2> GetRightWall() const {
+            // }
+            //
+            // const std::tuple<glm::mat4, glm::vec3, glm::vec2> GetLeftWall() const {
+            // }
+            //
+            // const std::tuple<glm::mat4, glm::vec3, glm::vec2> GetTopWall() const {
+            // }
+            //
+            // const std::tuple<glm::mat4, glm::vec3, glm::vec2> GetBottomWall() const {
+            // }
 
         private:
-            std::array<glm::vec3, 8> walls;
-            f32 sphereRadius;
-            std::array<glm::vec3, 2> spherePositionAndSpeed;
+            /** @brief 4 walls of a room represented by their normal and distance from the origin. */
+            std::array<std::pair<glm::vec3, glm::vec3>, 4> walls;
+
+            /** @brief Spheres represented by its speed, position and radius. */
+            std::array<std::tuple<glm::vec3, glm::vec3, f32>, 1> spheres;
     };
 
     class SandboxLayerSphere final : public Layer {
@@ -61,26 +90,28 @@ namespace Sandbox {
                 : app(Application::GetSingleton())
                 , audioSystem(CreateRef<AudioSystem>())
                 , camera(Camera::Create())
-                , sphere(1.0f, 100, 100)
-                , world(
-                      {
-                          glm::vec3(5, 0, 0),  // Left Wall.
-                          glm::vec3(-1, 0, 0), // Left Wall Normal.
-                          glm::vec3(-5, 0, 0), // Right Wall.
-                          glm::vec3(1, 0, 0),  // Right Wall Normal.
-                          glm::vec3(0, 5, 0),  // Top Wall.
-                          glm::vec3(0, -1, 0), // Top Wall Normal.
-                          glm::vec3(0, -5, 0), // Bottom Wall.
-                          glm::vec3(0, 1, 0),  // Bottom Wall Normal.
-                      },
-                      1.0f,
-                      {
-                          glm::vec3(0.0f, 0.0f, 0.0f),   // Sphere Position.
-                          glm::vec3(10.0f, 15.0f, 0.0f), // Sphere Speed.
-                      }) {
+                , sphere(1.0f, 100, 100) {
 
+                // Define the walls of the room as planes with their normal and distance from the origin.
+                const std::array<std::pair<glm::vec3, f32>, 4> walls = {
+                    std::make_pair<glm::vec3, f32>(glm::vec3(-1, 0, 0), 5.0f), // Left Wall.
+                    std::make_pair<glm::vec3, f32>(glm::vec3(1, 0, 0), 5.0f),  // Right Wall.
+                    std::make_pair<glm::vec3, f32>(glm::vec3(0, -1, 0), 5.0f), // Top Wall.
+                    std::make_pair<glm::vec3, f32>(glm::vec3(0, 1, 0), 5.0f),  // Bottom Wall.
+                };
+
+                // Define a single sphere with its initial position, speed and radius.
+                const std::array<std::tuple<glm::vec3, glm::vec3, f32>, 1> spheres = {
+                    std::make_tuple<glm::vec3, glm::vec3, f32>(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(10.0f, 15.0f, 0.0f), 1.0f), // Sphere.
+                };
+
+                // Create the physics world with the defined walls and spheres.
+                world = World(walls, spheres);
+
+                // Set initial camera position.
                 camera.SetPosition(glm::vec3(0.0f, 0.0f, 20.0f));
 
+                // Load audio clips.
                 fahAudioClip = audioSystem->LoadClip("assets/sounds/fahhh.wav");
                 akAudioClip = audioSystem->LoadClip("assets/sounds/csgo-ak.wav");
 
@@ -174,18 +205,22 @@ namespace Sandbox {
                     // Set the model matrix for the floor.
                     switch (i) {
                         case 0:
-                            model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -4.5f, 0.0f));
+                            model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, -5.0f, 0.0f));
+                            model = glm::scale(model, glm::vec3(5.0f));
                             break;
                         case 1:
-                            model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 5.5f, 0.0f));
+                            model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 5.0f, 0.0f));
+                            model = glm::scale(model, glm::vec3(5.0f));
                             break;
                         case 2:
-                            model = glm::translate(glm::mat4(1.0f), glm::vec3(4.5f, 0.0f, 0.0f));
+                            model = glm::translate(glm::mat4(1.0f), glm::vec3(5.0f, 0.0f, 0.0f));
                             model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+                            model = glm::scale(model, glm::vec3(5.0f));
                             break;
                         case 3:
-                            model = glm::translate(glm::mat4(1.0f), glm::vec3(-5.5f, 0.0f, 0.0f));
+                            model = glm::translate(glm::mat4(1.0f), glm::vec3(-5.0f, 0.0f, 0.0f));
                             model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+                            model = glm::scale(model, glm::vec3(5.0f));
                             break;
                     }
 
@@ -248,10 +283,10 @@ namespace Sandbox {
             Ref<VertexArray> floorVAO;
             std::vector<f32> floorVertices = {
                 // positions         // normals        // texture coords
-                5.0f,  -0.5f, 5.0f,  0.0f, 1.0f, 0.0f, 2.0f, 0.0f, //
-                -5.0f, -0.5f, 5.0f,  0.0f, 1.0f, 0.0f, 0.0f, 0.0f, //
-                -5.0f, -0.5f, -5.0f, 0.0f, 1.0f, 0.0f, 0.0f, 2.0f, //
-                5.0f,  -0.5f, -5.0f, 0.0f, 1.0f, 0.0f, 2.0f, 2.0f  //
+                1.0f,  0.0f, 1.0f,  0.0f, 1.0f, 0.0f, 1.0f, 0.0f, //
+                -1.0f, 0.0f, 1.0f,  0.0f, 1.0f, 0.0f, 0.0f, 0.0f, //
+                -1.0f, 0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, //
+                1.0f,  0.0f, -1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f  //
             };
             std::vector<u32> floorIndices = {
                 0, 1, 2, //
