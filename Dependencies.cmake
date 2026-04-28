5c46bff3a735a9b2281f5c59417998f054fdbe33
Raj <amritrjakacabu015@gmail.com>
2026-04-28 17:41:48 -0400

    Updating CMake Requirement to 4.3

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -4,6 +4,9 @@
 # GLFW
 find_package(glfw3 CONFIG REQUIRED)
 
+# GLAD (use vcpkg-provided port)
+find_package(glad CONFIG REQUIRED)
+
 # GLM
 find_package(glm CONFIG REQUIRED)
 
@@ -23,6 +26,3 @@ find_package(Stb REQUIRED)
 if(VULKYRIE_BUILD_TESTS)
     find_package(Catch2 CONFIG REQUIRED)
 endif()
-
-# GLAD (use vcpkg-provided port)
-find_package(glad CONFIG REQUIRED)
