c9ddf021433e6ff8671aa269dc6c0867ceacb4a4
Raj <amritrjakacabu015@gmail.com>
2026-04-29 23:32:47 -0400

    Starting initial work on BVH with dynamic AABB trees.

--- engine/include/physics/collision/broadphase/dynamic_aabb_tree.h
+++ engine/include/physics/collision/broadphase/dynamic_aabb_tree.h
@@ -4,34 +4,37 @@
 
 namespace Vulkyrie {
 
-    // struct DynamicAABBTreeNode final {
-    //     public:
-    //         static constexpr i32 NULL_NODE = -1;
-    //
-    //         Vulkyrie::AABB AABB;
-    //         i32 Height;
-    //
-    //         union {
-    //                 i32 ParentNodeID;
-    //                 i32 NextNodeID;
-    //         };
-    //
-    //         union {
-    //                 struct Children {
-    //                         i32 LeftChildNodeID;
-    //                         i32 RightChildNodeID;
-    //                 };
-    //                 Entity Entity;
-    //         };
-    //
-    //         DynamicAABBTreeNode()
-    // };
-    //
-    // class DynamicAABBTree final {
-    //     public:
-    //     private:
-    //         std::vector<DynamicAABBTreeNode> _nodes;
-    //         i32 _rootNodeId;
-    // };
+    struct DynamicAABBTreeNode final {
+        public:
+            static constexpr size_t NULL_NODE = -1;
+
+            Vulkyrie::AABB AABB;
+            size_t LeftChildID;
+            size_t RightChildID;
+
+            union {
+                    size_t ParentNodeID;
+                    size_t NextNodeID;
+            };
+
+            i32 Height;
+
+            DynamicAABBTreeNode();
+    };
+
+    class DynamicAABBTree final {
+        public:
+            DynamicAABBTree();
+
+            ~DynamicAABBTree() = default;
+
+            void AddObject(const AABB &aabb);
+            void RemoveObject(size_t nodeId);
+            void UpdateObject(size_t nodeId, const AABB &newAABB);
+
+        private:
+            std::vector<DynamicAABBTreeNode> _nodes;
+            size_t _rootNodeId;
+    };
 
 } // namespace Vulkyrie
--- engine/src/physics/collision/broadphase/dynamic_aabb_tree.cpp
+++ engine/src/physics/collision/broadphase/dynamic_aabb_tree.cpp
@@ -1,3 +1,18 @@
 #include "physics/collision/broadphase/dynamic_aabb_tree.h"
 
-namespace Vulkyrie {}
+namespace Vulkyrie {
+
+    DynamicAABBTree::DynamicAABBTree()
+        : _rootNodeId(0) {
+    }
+
+    void DynamicAABBTree::AddObject(const AABB &aabb) {
+    }
+
+    void DynamicAABBTree::RemoveObject(size_t nodeId) {
+    }
+
+    void DynamicAABBTree::UpdateObject(size_t nodeId, const AABB &newAABB) {
+    }
+
+} // namespace Vulkyrie
