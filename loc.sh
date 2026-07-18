0eb1c4beb5cc036489379efc50ffc2bc7867c6ce
Raj <amritrjakacabu015@gmail.com>
2026-07-17 23:25:04 -0400

    Excluding `.claude` directory from loc calculation.

--- loc.sh
+++ loc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cloc ./ --exclude-dir=.cache,.git,.vscode,.idea,build,vcpkg_installed --exclude-list-file=compile_commands.json
+cloc ./ --exclude-dir=.cache,.git,.vscode,.claude,.idea,build,vcpkg_installed --exclude-list-file=compile_commands.json
