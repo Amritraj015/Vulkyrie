619222f445acfb82fa0ec0cebff6013825fee97d
Raj <amritrjakacabu015@gmail.com>
2026-08-09 21:40:00 -0400

    Adding descriptions to the UUID class.

--- engine/include/core/types/uuid.h
+++ engine/include/core/types/uuid.h
@@ -6,17 +6,20 @@ namespace Vulkyrie {
 
     class UUID final {
     public:
+        /** @brief Creates a random UUID. */
         UUID();
+
+        /** @brief Creates a copy from a provided UUID. */
         UUID(u64 uuid);
+
         UUID(const UUID &) = default;
 
+        /** @brief Gets the stored UUID. */
         [[nodiscard]] VE_INLINE u64 GetUUID() const {
             return _uuid;
         }
 
-        bool operator==(const UUID &other) const {
-            return _uuid == other._uuid;
-        }
+        [[nodiscard]] VE_INLINE constexpr bool operator==(const UUID &other) const noexcept = default;
 
     private:
         u64 _uuid = 0;
